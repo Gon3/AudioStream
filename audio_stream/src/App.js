d8e3a719961e3c65f458f8cc1602f939674957cf
Gon3 <gamat174@gmail.com>
2024-09-07 21:46:37 -0400

    second commit - audio output change configured, still working on input

--- audio_stream/src/App.js
+++ audio_stream/src/App.js
@@ -1,32 +1,71 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { initializePeer, makeCall } from './P2PConnection';
+import { initializePeer, makeCall, setInputOnCall } from './P2PConnection';
 import './App.css';
 
 function App() {
   const [remoteStream, setRemoteStream] = useState(null);
   const [peerId, setPeerId] = useState('');
   const [userId, setUserId] = useState('');
+  const [output, setOutput] = useState("default");
+  const [input, setInput] = useState("default");
+  const [outputs, setOutputs] = useState([]);
+  const [inputs, setInputs] = useState([]);  
   const remoteAudioRef = useRef(null);
 
   useEffect(() => {
+    let outputsArr = []; 
+    let inputsArr = [];
+    navigator.mediaDevices
+    .enumerateDevices()
+    .then((devices) => {
+      devices.forEach((device) => {
+        if(device.kind === "audiooutput"){
+          //console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
+          outputsArr.push(<option key={device.deviceId} value={device.deviceId}>
+            {device.deviceId === "default" ? "Default - " : "" + device.label}
+          </option>);
+        } else if (device.kind === "audioinput"){
+          inputsArr.push(<option key={device.deviceId} value={device.deviceId}>
+            {device.deviceId === "default" ? "Default - " : "" + device.label}
+          </option>);
+        }
+        
+        });
+        setOutputs(outputsArr); 
+        setInputs(inputsArr)
+    });
+  }, []);
+
+  useEffect(() => {
+    initializePeer(userId, input, (stream) => {
+      setRemoteStream(stream);
+    });
+  }, [userId]);
+
+  useEffect(() => {
     if (remoteStream && remoteAudioRef.current) {
-      //console.log(remoteStream);
       remoteAudioRef.current.srcObject = remoteStream //plug stream into audio element  
     }
   }, [remoteStream]);
 
-  const handleEnter = () => {
-    initializePeer(userId, (stream) => {
+  useEffect(() => {
+    setInputOnCall(input, (stream) => {
       setRemoteStream(stream);
     });
-  }
+  }, [input]);
+
+  useEffect(() => {
+    if(remoteAudioRef.current){
+      remoteAudioRef.current.setSinkId(output);
+    }
+  }, [output]);
 
   const handleCall = () => {
-    makeCall(peerId, (stream) => {
+    makeCall(peerId, input, (stream) => {
       setRemoteStream(stream);
     });
   };
-
+  
   return (
     <div className="App">
       <h1>Audio Streamer</h1>
@@ -35,7 +74,6 @@ function App() {
         value={userId}
         onChange={(e) => setUserId(e.target.value)}
       />
-      <button onClick={handleEnter}>Enter</button>
       <br/>
       <input
         placeholder='Enter ID to connect to'
@@ -44,6 +82,23 @@ function App() {
         disabled={remoteStream}
       />
       <button onClick={handleCall} disabled={remoteStream}>Call</button>
+      <br />
+
+      <label>
+        Set audio input:
+      </label>
+      <select value={input} onChange={(e) => setInput(e.target.value)}>
+        {inputs}
+      </select>
+     
+      <br />
+      <label>
+        Set audio output:
+      </label>
+      <select value={output} onChange={(e) => setOutput(e.target.value)}>
+        {outputs}
+      </select>
+
       <audio ref={remoteAudioRef} autoPlay></audio>
     </div>
   );
--- audio_stream/src/P2PConnection.js
+++ audio_stream/src/P2PConnection.js
@@ -5,29 +5,36 @@ let peer = null;
 let localStream = null;
 let remoteStream = null;
 
-const initializePeer = (userId, callback) => {
+const setInputOnCall = (inputId, callback) => {
+    if(peer){
+       peer.on('call', (call) => { //what to do when receiving calls
+        console.log(inputId);
+            navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => { //get stream from current audio input (microphone)
+                localStream = stream; 
+                call.answer(stream);
+                call.on('stream', (remoteAudioStream) => {
+                    remoteStream = remoteAudioStream;
+                    callback(remoteAudioStream); //send audio stream from remote peer
+                })
+            })  
+        }) 
+    }
+}
+
+const initializePeer = (userId, inputId, callback) => {
     peer = new Peer(userId);//create peer object
 
     peer.on('open', (id) => { 
         console.log('My peer ID is: ' + id);
     });
 
-    peer.on('call', (call) => { //handle any incoming calls
-        navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => { //get stream from current audio input (microphone)
-            localStream = stream; 
-            call.answer(stream);
-            call.on('stream', (remoteAudioStream) => {
-                remoteStream = remoteAudioStream;
-                callback(remoteAudioStream); //send audio stream from remote peer
-            })
-        })
-    })
+    setInputOnCall(inputId, callback); 
 }
 
-const makeCall = (userId, callback) => {
-    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+const makeCall = (userId, inputId, callback) => { //what to do when initiating call
+    navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => {
         localStream = stream; 
-        const call = peer.call(userId, stream); //how will we continue to send localstream
+        const call = peer.call(userId, stream); 
         call.on('stream', (remoteAudioStream) => {
             remoteStream = remoteAudioStream;
             callback(remoteAudioStream); //send audio stream from remote peer
@@ -35,7 +42,9 @@ const makeCall = (userId, callback) => {
     })
 }
 
+
+
 const getLocalStream = () => localStream;
 const getRemoteStream = () => remoteStream;
 
-export {initializePeer, makeCall, getLocalStream, getRemoteStream};
+export {initializePeer, makeCall, setInputOnCall, getLocalStream, getRemoteStream};
